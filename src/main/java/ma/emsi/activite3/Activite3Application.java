23572649dda042cbb63bddd1841d21cb75bbe018
elyamanihajar <hajar.elyamani03@gmail.com>
2025-05-15 10:17:46 +0100

    Security

--- src/main/java/ma/emsi/activite3/Activite3Application.java
+++ src/main/java/ma/emsi/activite3/Activite3Application.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
 @SpringBootApplication
-public class Activite3Application implements CommandLineRunner {
+public class Activite3Application {
     @Autowired
     private PatientRepository patientRepository;
 
@@ -18,12 +21,27 @@ public class Activite3Application implements CommandLineRunner {
         SpringApplication.run(Activite3Application.class, args);
     }
 
-    @Override
+    /*@Override
     public void run(String... args) throws Exception {
-        //patientRepository.save(new Patient(null,"Mehdi",new Date(),false,23));
-        //patientRepository.save(new Patient(null,"Hajar",new Date(),true,20));
-        //patientRepository.save(new Patient(null,"Hind",new Date(),false,30));
-        //patientRepository.save(new Patient(null,"Bouchra",new Date(),true,40));
+        patientRepository.save(new Patient(null,"Mehdi",new Date(),false,23));
+        patientRepository.save(new Patient(null,"Hajar",new Date(),true,20));
+        patientRepository.save(new Patient(null,"Hind",new Date(),false,30));
+        patientRepository.save(new Patient(null,"Bouchra",new Date(),true,40));
 
+    }*/
+
+    //@Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
+        return args -> {
+            patientRepository.save(new Patient(null,"Mehdi",new Date(),false,23));
+            patientRepository.save(new Patient(null,"Hajar",new Date(),true,20));
+            patientRepository.save(new Patient(null,"Hind",new Date(),false,30));
+            patientRepository.save(new Patient(null,"Bouchra",new Date(),true,40));
+        };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/ma/emsi/activite3/web/PatientController.java
+++ src/main/java/ma/emsi/activite3/web/PatientController.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class PatientController {
     private PatientRepository patientRepository;
 
-    @GetMapping("/index")
+    @GetMapping("/user/index")
     public String index(Model model, @RequestParam(name = "page",defaultValue = "0") int page,
                         @RequestParam(name = "size",defaultValue = "4")int size,
     @RequestParam(name = "keyword",defaultValue = "")String kw) {
@@ -33,28 +33,28 @@ public class PatientController {
         return "patients";
     }
 
-    @GetMapping("/delete")
+    @GetMapping("/admin/delete")
     public String delete(Long id, String keyword, int page){
         patientRepository.deleteById(id);
-        return "redirect:/index?page="+page+"&keyword="+keyword;
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
     }
 
-    @GetMapping("/formPatients")
+    @GetMapping("/admin/formPatients")
     public String formPatients(Model model) {
         model.addAttribute("patient", new Patient());
         return "formPatients";
     }
 
-    @PostMapping(path = "/save")
+    @PostMapping(path = "/admin/save")
     public String save(Model model, @Valid Patient patient, BindingResult bindingResult,
                        @RequestParam(defaultValue = "") String keyword,
                        @RequestParam(defaultValue = "0") int page) {
         if(bindingResult.hasErrors()) return "formPatients";
         patientRepository.save(patient);
-        return "redirect:/index?page="+page+"&keyword="+keyword;
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
     }
 
-    @GetMapping("/editPatient")
+    @GetMapping("/admin/editPatient")
     public String editPatient(Model model, Long id, String keyword, int page) {
         Patient patient = patientRepository.findById(id).orElse(null);
         if(patient == null) throw new RuntimeException("Patient not found");
@@ -62,5 +62,10 @@ public class PatientController {
         model.addAttribute("keyword", keyword);
         model.addAttribute("page", page);
         return "editPatient";
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "redirect:/user/index";
     }
 }
